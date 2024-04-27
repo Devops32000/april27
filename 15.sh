02253a309203529db77a854e01b4b2bdd6fac00a
Rajesh <rajesh23@gmail>
2024-04-27 16:28:13 +0530

    practicing shell scripting

---
+++ 15.sh
@@ -0,0 +1,8 @@
+course="Devops from current script"
+
+echo "before calling other script : $course"
+echo "process ID of current script: $$"
+
+./16.sh
+
+echo "After calling other script: $course"
---
+++ 16.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+course="devops from other script"
+
+echo "variable value from other script:$course"
+echo "process ID of the other script: $$"
+
