90788d149dd36a74c8ee6e84b9b010291a9885ec
Rajesh <rajesh23@gmail>
2024-04-27 16:30:19 +0530

    practicing shell scripting

--- 15.sh
+++ 15.sh
@@ -3,6 +3,6 @@ course="Devops from current script"
 echo "before calling other script : $course"
 echo "process ID of current script: $$"
 
-./16.sh
+source ./16.sh
 
 echo "After calling other script: $course"
