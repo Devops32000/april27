8e2db74da5b7d36a447d19d673106dac00ad7b8f
Rajesh <rajesh23@gmail>
2024-04-27 11:55:46 +0530

    practicing shell scripting

--- ysu.sh
+++ ysu.sh
@@ -8,5 +8,6 @@ echo "number of variables passed $#"
 echo "This is the scritp name $0"
 echo "Which user is running the script $USER"
 echo "process ID of the last bg process ID $!"
+echo "Process id of the current shell script $$"
 
 
