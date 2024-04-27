6c4d59232dbe6a4595eb7d4d9e9876ace49a1082
Rajesh <rajesh23@gmail>
2024-04-27 13:21:01 +0530

    practicing shell scripting

--- ins.sh
+++ ins.sh
@@ -26,7 +26,7 @@ else
     echo "You are a super user"
 fi 
 
-dnf install mysqll -y &>>$logfile
+dnf install mysql -y &>>$logfile
 validate $? "Installation of mysql"
 
 dnf install git -y &>>$logfile
