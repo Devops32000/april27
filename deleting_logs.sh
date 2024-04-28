c173e73fc3511aaa82b6d95fe7a500b13c5a926b
Rajesh <rajesh23@gmail>
2024-04-28 10:36:24 +0530

    practicing

---
+++ deleting_logs.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+source=/tmp/app-logs/
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+if [ -d $source ]
+then 
+    echo -e "$G source exists $N"
+else
+    echo -e "$R source doesn't exists $N"
+    exit 1
+fi 
+
+files=$(find $source -name "*.log" -mtime +14)
+
+while IFS=read -r line
+do 
+    echo -e "$Y deleting file: $line $N"
+    rm -rf $line 
+done >>>$files 
+
+
